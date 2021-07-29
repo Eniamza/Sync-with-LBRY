732713ccc462632e5c6a55cf3aa369e7b7a20d7d
Eniamza <eniamza0@gmail.com>
2021-07-30 04:20:15 +0600

    Feat: Add option selection in index

--- index.js
+++ index.js
@@ -2,14 +2,25 @@ const path = require("path")
 const fs = require("fs")
 const chalk = require("chalk")
 const genConfig = require("./lib/misc/genConfig")
+const readlineSync = require("readline-sync")
+const utxo = require("./lib/lbry/utxo")
 
 
 console.log(chalk.yellowBright("Welcome to Sync with LBRY!"))
+
+let options = ["Sync from YouTube","Exit"]
+let chosenOp 
 
 let execEverything = async function(){
 
 
   await genConfig();
+  chosenOp = readlineSync.keyInSelect(options, 'Select your Destiny!');
+
+  if(chosenOp === 0){
+    console.log(chalk.greenBright("Youtube Sync Mode Selected."))
+    await utxo.checkUTXO();
+  }
 
 
 }
